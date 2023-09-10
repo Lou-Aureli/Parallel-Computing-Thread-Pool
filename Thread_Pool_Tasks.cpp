9df46daf84f1868d46a295611e69082216f6b452
Lou-Aureli <nai.ian.2K15@gmail.com>
2023-09-10 15:44:00 -0500

    Spinning threads now compleat

--- Thread_Pool_Tasks.cpp
+++ Thread_Pool_Tasks.cpp
@@ -12,8 +12,10 @@
 
 struct work_pack
 {
-    int             id, num;  // User Query
-    pthread_cond_t* wait; // Used to make the thread wait for work
+    int               id, num;  // User Query
+    pthread_t         tid;
+    pthread_cond_t*   wait;     // Used to make the thread wait for work
+    pthread_mutex_t*  lock;     // 
     work_pack(int i_id = 0, int i_num = 1) 
     {
         id = i_id;
@@ -21,46 +23,85 @@ struct work_pack
         wait = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
         pthread_cond_init(wait, NULL);
     }
+    ~work_pack() { pthread_cond_destroy(wait); }
 };
 
 struct task_manager
 {
     pthread_mutex_t*      lock;   // Global Lock
-    std::queue<work_pack> prog;   // Stores all the working threads
-    std::queue<work_pack> avail;  // Stores all the available threads
-    task_manager() { lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t)); }
+    std::queue<work_pack*> prog;   // Stores all the working threads
+    std::queue<work_pack*> avail;  // Stores all the available threads
+    task_manager() 
+    { 
+        lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t)); 
+        pthread_mutex_init(lock, NULL);
+    }
 };
 
 struct thread_pack
 {
-    int id;
-    void* work;
-    void* global;
+    pthread_t   tid;
+    void*       work;
+    void*       global;
+    thread_pack(task_manager* i_g, work_pack* i_w)
+    {
+      work = (void*)i_w;
+      global = (void*)i_g;
+    }
 };
 
 void pause_resume();
 void* prime_thread(void* args);
+void print_usage();
 
 int main(int argc, char** argv)
       // usage: /.Thread_Pool [Opt. Num_Threads]
 {
-    task_manager thread_pool;
+    task_manager* thread_pool = new task_manager;
     int num_thread = 10;
     if(argc >= 2)
       num_thread = std::stoi(argv[1]);
-
       // Spin specified number of threads and have them wait for tasks
     for(int i = 0; i < num_thread; ++i)
     {
-      struct work_pack* tmp = (struct work_pack*)malloc(sizeof(struct work_pack*));
-
+      work_pack* tmp_w = new work_pack(i);
+      thread_pack* tmp = new thread_pack(thread_pool, tmp_w);
+      if (pthread_create(&(tmp->tid), NULL, prime_thread, (void*)tmp) != 0)
+          perror("pthread_create(Chat_Room)");
+      thread_pool->avail.push(tmp_w);
     }  
       // Await user input in a loop. If user specifies a number assign a thread
         // If user specifies an action, do the action
-      
+    print_usage();
+    std::string buffer;
+    bool isPaused = false;
+    while(std::cin >> buffer)
+    {
+      if(buffer.compare("stop") == 0) 
+        break;
+      else if(buffer.compare("pause") == 0 && isPaused == false) 
+      {
+        pause_resume(); 
+        isPaused = true;
+      }
+      else if(buffer.compare("resume") == 0 && isPaused == true) 
+      {
+        pause_resume(); 
+        isPaused = false;
+      }
+
+      printf("Available Threads: %ld\n", thread_pool->avail.size());
+    }
       // When Stop is specified AND TASK MANAGER IS EMPTY, break out and clean up
 
     return 0;
+}
+
+void print_usage()
+{
+    printf("Usage(case sensitive):\n\t[#] - Input a number to see if it's prime or not\n");
+    printf("\tpause - pause all threads\n\tresume - resume all threads(does nothing if not paused)\n");
+    printf("\tstop - end the program\n\thelp - see this menue again\n");
 }
 
 void pause_resume()
@@ -73,7 +114,7 @@ void* prime_thread(void* args)
     thread_pack* arg = (thread_pack*)args;
     work_pack* work = (work_pack*)arg->work;
     task_manager* global = (task_manager*)arg->global;
-
+    work->tid = arg->tid;
     while(1)
     {
           //Lock the mutex then wait for work
